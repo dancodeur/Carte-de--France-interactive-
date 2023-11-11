234471292c6f3ba20964c87c39a9c3899fbd8617
ELENGA Dan David <elengadan@gmail.com>
2023-11-11 02:41:03 +0100

    ajout de la fonction aide et smiles avec des intéractions

--- js/index.js
+++ js/index.js
@@ -2,6 +2,12 @@ const path=document.querySelectorAll("path");
 const dataSvg=[{nom:"",population:"",lien:""}];
 const displayData=document.querySelector("[data-res]");
 
+
+const invitation=document.querySelector("[data-aide]");
+const compris=document.querySelector("[data-compris]");
+
+compris.style.display="none";
+
 /***modification view port svg */
 
 
@@ -11,6 +17,14 @@ path.forEach((e)=>{
     
     // console.log(e.dataset);
     e.addEventListener("click",(y)=>{
+        
+        invitation.style.display="none";
+        compris.style.display="block";
+
+        setTimeout(()=>{
+            compris.innerHTML=`<span data-compris> &#128519; si vous souhaitez consulter d'autres réalisations que j'ai faite voici le lien de mon <a href="https://dancodeur.github.io/dan-portfolio/" target="_blank" style="color:rgb(108, 208, 116)">portfolio.</a></span>`;
+        },10000);
+
         y.target.classList.add("change");
         console.log(e.dataset.info);
         console.log(e.dataset);
@@ -25,7 +39,7 @@ path.forEach((e)=>{
             displayData.innerHTML=`
               <li><span style="color:black; font-weight:bold;">Région sélectionnée:</span> ${data.nom}</li>
               <li><span style="color:black; font-weight:bold;">Population:</span> ${data.population} habitants</li>
-              <li><span style="color:black; font-weight:bold;">Source insee:</span> <a href="${data.lien}" target="_blank" style="color:rgb(108, 208, 116)">en savoir plus.</a></li>
+              <li><span style="color:black; font-weight:bold;">Plus d'infos:</span> <a href="${data.lien}" target="_blank" style="color:rgb(108, 208, 116)">Insee.fr</a></li>
             `;
         });
     });
