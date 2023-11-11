031bd69ec642b529e10b6fc6c53ad24e97a8cb14
ELENGA Dan David <elengadan@gmail.com>
2023-11-11 01:40:17 +0100

    mise à jour des données de la carte avec les data-attribute et methode affichage des données

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const path=document.querySelectorAll("path");
-const dataSvg=[];
+const dataSvg=[{nom:"",population:"",lien:""}];
 const displayData=document.querySelector("[data-res]");
+
+/***modification view port svg */
+
 
 
 
@@ -10,12 +13,19 @@ path.forEach((e)=>{
     e.addEventListener("click",(y)=>{
         y.target.classList.add("change");
         console.log(e.dataset.info);
+        console.log(e.dataset);
 
-        dataSvg.push(e.dataset.info);
+        let nameRegion=e.dataset.info;
+        let Population=e.dataset.population;
+        let link=e.dataset.lienInsee;
+
+        dataSvg.push({nom:nameRegion,population:Population,lien:link});
         
         dataSvg.forEach((data)=>{
             displayData.innerHTML=`
-              <li>${data}</li>
+              <li><span style="color:black; font-weight:bold;">Région sélectionnée:</span> ${data.nom}</li>
+              <li><span style="color:black; font-weight:bold;">Population:</span> ${data.population} habitants</li>
+              <li><span style="color:black; font-weight:bold;">Source insee:</span> <a href="${data.lien}" target="_blank" style="color:rgb(108, 208, 116)">en savoir plus.</a></li>
             `;
         });
     });
